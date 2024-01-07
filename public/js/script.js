d84f277b173a4da95ef9b8f61d9b7da4c691874c
subhajitghosh_ <orrin2op@gmail.com>
2024-01-07 19:01:11 +0530

    update

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,27 @@ crossbtn.addEventListener('click',()=>{
     let navbar=document.querySelector('.navbar');
     navbar.classList.remove('active-navbar');
 })
+
+// move signup and login
+let login=document.querySelector("#login");
+login.addEventListener('click',()=>{
+    let wrapper=document.querySelector(".wrapper");
+    wrapper.style.transform = 'translateX(-102%)';
+
+    let signup=document.querySelector(".signup");
+    signup.style.opacity=0;
+
+    let login=document.querySelector(".login");
+    login.style.opacity='100%';
+})
+let signup=document.querySelector("#signup");
+signup.addEventListener('click',()=>{
+    let wrapper=document.querySelector(".wrapper");
+    wrapper.style.transform = 'translateX(0%)';
+
+    let signup=document.querySelector(".signup");
+    signup.style.opacity='100%';
+
+    let login=document.querySelector(".login");
+    login.style.opacity=0;
+})
