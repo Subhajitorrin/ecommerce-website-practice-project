06c0319677f568525e8105c7323f7a534eb96a31
subhajitghosh_ <orrin2op@gmail.com>
2024-01-09 19:07:25 +0530

    update

--- public/js/script.js
+++ public/js/script.js
@@ -27,7 +27,7 @@
         totalPriceShow.innerHTML = `<b>${totalPrice}</b>`;
         shopingCostShow.innerHTML = `<b>${totalPrice}</b>`;
     }
-    priceCalculationOfCart();
+    // priceCalculationOfCart();
 
     // remove card
     let cardCrossList = document.querySelectorAll('.card-cross')
@@ -38,4 +38,57 @@
             priceCalculationOfCart();
         })
     });
+}
+
+
+// fetch products
+{
+    const jsonFilePath = '../assets/productDetails.json';
+    fetch(jsonFilePath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // code here
+            let homeMainSection = document.querySelector(".home-main");
+            for (let i = 0; i < data.length; i++) {
+                let card = createProductCard(data[i].img, data[i].name, data[i].price);
+                homeMainSection.appendChild(card);
+            }
+        })
+
+    //   function to create product cards
+    function createProductCard(img, name, price) {
+        const cardContainer = document.createElement('div');
+        cardContainer.className = 'home-cards';
+
+        const imgContainer = document.createElement('div');
+        imgContainer.className = 'home-img-container';
+        const imgElement = document.createElement('img');
+        imgElement.src = img;
+        imgContainer.appendChild(imgElement);
+
+        const productNameElement = document.createElement('div');
+        productNameElement.className = 'home-product-name';
+        productNameElement.innerHTML = `<b>${name}</b>`;
+
+        const productPriceElement = document.createElement('div');
+        productPriceElement.className = 'home-product-price';
+        productPriceElement.textContent = price;
+
+        const addToCartBtn = document.createElement('button');
+        addToCartBtn.className = 'add-to-cart-btn';
+        addToCartBtn.textContent = 'Add to cart';
+
+        // Append elements to the card container
+        cardContainer.appendChild(imgContainer);
+        cardContainer.appendChild(productNameElement);
+        cardContainer.appendChild(productPriceElement);
+        cardContainer.appendChild(addToCartBtn);
+
+        return cardContainer;
+    }
 }
